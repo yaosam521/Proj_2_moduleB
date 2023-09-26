05bdec4ca322febd8cdb7c21643beb81a30fcdbc
Sam Yao <yaosam.521@gmail.com>
2023-09-25 21:06:56 -0500

    Added a Table View to move between Modules

---
+++ AudioLabSwift-5_FFTAudioGraphing/AudioLabSwift/ModuleAViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ModuleAViewController.swift
+//  AudioLabSwift
+//
+//  Created by Sam Yao on 9/25/23.
+//  Copyright © 2023 Eric Larson. All rights reserved.
+//
+
+import UIKit
+import Metal
+import AVFoundation
+
+class ModuleAViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
